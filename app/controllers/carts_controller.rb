026a3daf9e9bf017e8b32f959192d2746cf1dfc1
tomoki828 <pico4828@gmail.com>
2020-04-17 10:16:37 +0900

    カートの一覧機能の大枠作成
    カートの一覧機能を作成。
    quantityを表示できておらず改善が必要。
    また、showに実装していた更新と削除機能をこちらに移管する必要がある。

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,9 +3,29 @@ class CartsController < ApplicationController
   before_action :set_cart_item!,   only: :add_item
   before_action :setup_cart_item!, only: [:update_item, :delete_item]
 
+  def index
+    @tiger   = Mask.find_by(id: 1)
+    @lion    = Mask.find_by(id: 2)
+    @cheetah = Mask.find_by(id: 3)
+    @cat     = Mask.find_by(id: 4)
+
+    @products = []
+    carts = Cart.all.order("created_at DESC")
+      carts.each do |cart|
+        mask = cart.masks
+          mask.each do |mask|
+            product = {}
+            product[:name]  = mask[:name]
+            product[:image] = mask[:image]
+            product[:price] = mask[:price]
+            @products << product
+          end
+      end
+  end
+  
   def show
-    cart = Cart.find(params[:id])
-    @mask = cart.masks
+    # cart = Cart.find(params[:id])
+    # @mask = cart.masks
   end
 
   # 商品一覧画面から、「商品購入」を押した時のアクション
@@ -28,7 +48,9 @@ class CartsController < ApplicationController
 
   # カート詳細画面から、「削除」を押した時のアクション
   def delete_item
+    @cart = Cart.find(params[:id])
     @cart_item.destroy
+    @cart.destroy
     redirect_to root_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "masks#index"
   resources :masks, only: [:show]
-  resources :carts, only: [:show]
+  resources :carts, only: [:index, :show]
 
   post '/add_item' => 'carts#add_item'
   post '/update_item' => 'carts#update_item'
