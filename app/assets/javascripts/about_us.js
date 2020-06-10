75938e94e86c4c5adee20c723f5ae9f9135b2c07
tomoki828 <pico4828@gmail.com>
2020-06-10 18:05:41 +0900

    ABOUT_US のアニメーション作成②
    ・layer2完成。ただしlayer1に引き続いてレスポンシブデザインにはなっていない。

--- app/assets/javascripts/about_us.js
+++ app/assets/javascripts/about_us.js
@@ -4,14 +4,20 @@ $(function() {
       opacity: '1'
         }, 400, 'linear' );
   }
+  let addFront = function(){
+    $('.aboutus__front').animate({ 
+      opacity: '1'
+        }, 400, 'linear' );
+  }
 
   $(window).on('load', function(){
-
-    // $('.aboutus__content--left').animate({ 
-    //   opacity: '1'
-    //     }, 400, 'linear' );
-    // $('.aboutus__layer--1').addClass('is-active');
-    // setTimeout(addContent, 1500);
-    $('.aboutus__layer--2').addClass('is-active');
+    $('.aboutus__content--left').animate({ 
+      opacity: '1'
+        }, 400, 'linear' );
+    $('.aboutus__layer--1').addClass('is-active').delay(1900).queue(function() {
+      $('.aboutus__layer--2').addClass('is-active').dequeue();
+    });
+    setTimeout(addContent, 1500);
+    setTimeout(addFront, 3300);
   });
 });
